630fc362cfd5d80afe8822fe761abe48e513aa8d
Mihir Sabhaya <mihir.roxontechnologies@gmail.com>
2026-03-07 12:29:40 +0530

    fix nextjs standalone build for hostinger

--- next.config.mjs
+++ next.config.mjs
@@ -1,5 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  output: 'standalone',
   async rewrites() {
     const isProd = process.env.NODE_ENV === 'production';
     const backend =
